7afafbe3c699d50a9ea638ee8b3c1c152c2f4084
even <EvenYan>
2018-07-23 22:25:50 +0800

    添加自动生成摘要的功能

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 # Create your models here.
 from django.urls import reverse
+import markdown
+from django.utils.html import strip_tags
 
 
 class Category(models.Model):
@@ -27,11 +29,30 @@ class Post(models.Model):
     excerpt = models.CharField(max_length=200, blank=True)
     category = models.ForeignKey(Category)
     tags = models.ManyToManyField(Tag, blank=True)
+    views = models.PositiveIntegerField(default=666)
 
     author = models.ForeignKey(User)
 
+    def save(self, *args, **kwargs):
+        # 如果没有填写摘要
+        if not self.excerpt:
+            # 首先实例化一个 Markdown 类，用于渲染 body 的文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            # 先将 Markdown 文本渲染成 HTML 文本
+            # strip_tags 去掉 HTML 文本的全部 HTML 标签
+            # 从文本摘取前 54 个字符赋给 excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post, self).save(*args, **kwargs)
+
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
 
     class Meta:
         ordering = ['-create_time']
