8850e5133e76e7a31bec56532092e603d34b9495
even <EvenYan>
2018-12-11 18:17:42 +0800

    将首页改成VS Code官网风格

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -79,21 +79,23 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog',
-        'USER': 'root',
-        'PASSWORD': '733@755Go',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'blog',
+            'USER': 'root',
+            'PASSWORD': '733@755Go',
+            'HOST': '127.0.0.1',
+            'PORT': '3306'
+        }
 }
 
 
