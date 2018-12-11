b11625d9ee06c501cd9079140d4201880be7eb83
even <EvenYan>
2018-12-11 18:42:11 +0800

    修改导航栏链接

--- blog/urls.py
+++ blog/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
     url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name="tag"),
     # url(r'^search/$', views.search, name='search'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^about/$', views.about, name='about'),
 
 ]
